75e50649baa8bc04a73fde1acfe06de602b9449c
Aksel Allas <aksel.allas@mil.ee>
2020-12-08 22:28:51 +0200

    Succesful demo version

--- main.py
+++ main.py
@@ -77,29 +77,29 @@ drone.navdata_ready.wait()
 drone.video_ready.wait()
 
 # yolo preparation
-config = 'tiny_yolo/yolov3-tiny.cfg'
-weights = 'tiny_yolo/yolov3-tiny.weights'
+config = 'tiny_yolo/yolov3.cfg'
+weights = 'tiny_yolo/yolov3.weights'
 net, background = tiny_yolo.yolo_update.init_yolo(config, weights)
 
 fps = 0
 cv2.namedWindow('Detection and tracking')
 last_frame_shown = time.time()
 time_s = time.time()
+go_back = False
 
 try:
     # threading.Thread(target=recordVideo, args=(drone,)).start()
     # navThread = threading.Thread(target=printNavdata, args=(drone,)).start()
-    out = cv2.VideoWriter('test-flight-with-yolo.mp4', 0x7634706d, 30.0, (640,360))
+    out = cv2.VideoWriter('test-flight-with-yolo-4.mp4', 0x7634706d, 2.0, (640,360))
     with suppress(KeyboardInterrupt):
         land_time = time.time() + 15
         while True:
             # image = cv2.resize(image,(608,608), interpolation = cv2.INTER_AREA)
+            if not drone.state.fly_mask:
+                drone.takeoff()
 
             image = drone.frame
             fps, last_frame_shown = fps_calc(time.time(), last_frame_shown)
-
-            while not drone.state.fly_mask:
-                drone.takeoff()
 
             if time.time() > land_time:
                 while drone.state.fly_mask:
@@ -114,12 +114,20 @@ try:
                 image = tiny_yolo.yolo_update.add_size(image, w)
 
                 ## Control drone
-                if w > 220:
-                    drone.move(backward=0.3)
+                if drone.state.fly_mask:
+                    if w < 170:
+                        go_back=False
+                    if w >= 170:
+                        drone.move(backward=0.9)
+                        go_back=True
+                    else:
+                        drone.move(forward=0.24)
+            else:
+                if go_back:
+                    drone.move(backward=0.9)
                 else:
-                    t_end = time_s + 3
-                    if time.time() < t_end:
-                        drone.move(forward=0.3)
+                    drone.move(forward=0.28)
+
 
             image = add_fps(image, fps)
             cv2.imshow('Detection and tracking', image)
